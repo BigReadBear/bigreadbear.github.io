11cde6102ad35acca21dc49705d0d0c24eec98e2
BigReadBear <71031668+BigReadBear@users.noreply.github.com>
2020-10-25 17:08:00 -0400

    Update clubMeNow.js

--- js/clubMeNow.js
+++ js/clubMeNow.js
@@ -92,7 +92,7 @@ function displayClubDistanceEntryForm(c) {
 // replace the current "clubs" array with the previous one
 function undoLastShot() {
         clubs = JSON.parse(localStorage.getItem("clubs"));
-	window.location.href = "clubs = JSON.parse(localStorage.getItem("clubs"));clubDistanceList.html";
+	window.location.href = "clubs = JSON.parse(localStorage.getItem("clubs")); https://bigreadbear.github.io/assignments/assignment04/clubDistanceList.html";
 }
 
 // create a new (default) "clubs" array
