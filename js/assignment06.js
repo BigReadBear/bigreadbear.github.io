e55a83aa561f4344f82c483577559c1b9555700c
BigReadBear <71031668+BigReadBear@users.noreply.github.com>
2020-11-30 16:17:42 -0500

    Update assignment06.js

--- js/assignment06.js
+++ js/assignment06.js
@@ -15,6 +15,7 @@ var loans = [
   function loadDoc() {
     
     // pre-fill defaults for first loan year
+    // change all the html selectors to jquery selectors and add the appropiate jquary attributes 
     var defaultYear = loans[0].loan_year;
     $("#loan_year0" + 1).val(defaultYear++);
     var defaultLoanAmount = loans[0].loan_amount;
@@ -24,7 +25,7 @@ var loans = [
     var loanWithInterest = loans[0].loan_amount * (1 + loans[0].loan_int_rate);
     $("#loan_bal0" + 1).text(toMoney(loanWithInterest));
     
-    // pre-fill defaults for other loan years
+    // pre-fill defaults for other loan years using jquery and add the appropiate attributes 
     for(var i=2; i<6; i++) {
       $(`#loan_year0${i}`).val(defaultYear++);
       $(`#loan_year0${i}`).attr("disabled","true");
@@ -34,130 +35,157 @@ var loans = [
       $(`#loan_int0${i}`).attr("disabled","true");
       $(`#loan_int0${i}`).css({"backgroundColor":"grey","color":"white"});
       loanWithInterest = (loanWithInterest + defaultLoanAmount) * (1 + defaultInterestRate);
-      $("#loan_bal0" + i).text(toMoney(loanWithInterest));
+      $(`#loan_bal0` + i).text(toMoney(loanWithInterest));
       } // end: "for" loop
     
     // all input fields: select contents on focus
     $("input[type=text]").focus(function() {
       $(this).select();
-      $(this).css("background-color", "yellow");
+      $(this).css("background-color", "yellow"); //focussed field set to yellow
     }); 
-    $("input[type=text]").blur(function() {
-      $(this).css("background-color", "white");
+    $("input[type=text]").blur(function() { 
+      $(this).css("background-color", "white"); //out of focus object sit to white
       updateLoansArray();
     });
     
     // set focus to first year: messes up codepen
-    $("#loan_year01").focus();
+    // $("#loan_year01").focus();
 
   } // end: function loadDoc()
   
-  let updateForm = () => {
+  //fucntion to update the form, aka the values displayed in the form
+  function updateForm() {
     loanWithInterest = 0;
-    let totalAmt = 0;
+    let total = 0;
+    //loops through the text fields
     for(i=1;i<6;i++){
-      $(`#loan_year0${i}`).val(loans[i-1].loan_year);
-      let amt = loans[i-1].loan_amount
+      $(`#loan_year0${i}`).val(loans[i-1].loan_year); //set value to previous 
+      let amt = loans[i-1].loan_amount; 
       $(`#loan_amt0${i}`).val(amt);
-      totalAmt+= parseFloat(amt);
+      total+= parseFloat(amt); 
       $(`#loan_int0${i}`).val(loans[i-1].loan_int_rate);
       loanWithInterest = (loanWithInterest + parseFloat(amt)) * (1 + loans[0].loan_int_rate);
 
       $("#loan_bal0" + i).text(toMoney(loanWithInterest));
     }
-    int = loanWithInterest-totalAmt;
-    $(`#loan_int_accrued`).text(toMoney(int));
+    int = loanWithInterest-total;
+    $(`#loan_int_accrued`).text(toMoney(int)); //loan interest accrued, bottom right of the form to money 
   }
   
+//existing function needed to round ints to 2 decimals needed for the tomoney function 
   function toComma(value) {
       return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
   }
 
-  let toMoney = (value) =>{
+//simple function needed to change values to monetary values, which i created because otherwise the numbers were all over the place and it looked hideous
+ function toMoney (value) {
     return `\$${toComma(value.toFixed(2))}`;
   }
   
+  //function to validate and update the form if correct values are entered
   function updateLoansArray() {
-    let valid = true;
-    let yearP = /^(19|20)\d{2}$/;
-    let amtP = /^([1-9][0-9]*)+(.[0-9]{1,2})?$/;
-    let intP = /^(0|)+(.[0-9]{1,5})?$/;
+    let valid = true; //initialize boolean to store valid or invalid 
+    //regular expressions
+    let year = /^(19|20)\d{2}$/; //only allow the last two centuries to be inputted
+    let amt = /^([1-9][0-9]*)+(.[0-9]{1,2})?$/; //only positive integers and 2 or 1 decimal number
+    let int = /^(0|)+(.[0-9]{1,5})?$/; //only allow decimals up to 5 points
 
-    if(!yearP.test($(`#loan_year01`).val())){
+    //test the input field witht he regexpress, set to valid is true when valid otherwise invalid bgcolor is red 
+    if(!year.test($(`#loan_year01`).val())){
       valid = false;
-      $(`#loan_year01`).css("background-color", "red");
+      $('#loan_year01').css("background-color", "red");
     }
-    
+    //test the input field witht he regexpress, set to valid is true when valid otherwise invalid bgcolor is red 
+    //different than the others because we can input all of the filed in this colllumn thus we need to check every input field of the loan amount
+    //hence the for loop
     for (i = 1; i < 6; i++) {
-      if(!amtP.test($(`#loan_amt0${i}`).val())){
+      if(!amt.test($(`#loan_amt0${i}`).val())){
         valid = false;
-        $(`#loan_amt0${i}`).css("background-color", "red");
+        $('#loan_amt0${i}').css("background-color", "red");
       } 
     }
-
-    if(!intP.test($(`#loan_int01`).val())){
+    //test the input field witht he regexpress, set to valid is true when valid otherwise invalid bgcolor is red 
+    if(!int.test($(`#loan_int01`).val())){
       valid = false;
-      $(`#loan_int01`).css("background-color", "red");
+      $('#loan_int01').css("background-color", "red");
     }
 
+    //when all the input fields are correct, update the loansarray and store correct values
     if(valid){
       loans[0].loan_year = parseInt($("#loan_year01").val());
       for(var i=1; i<5; i++) {
         loans[i].loan_year = loans[0].loan_year + i;
       }
       for(i = 1; i<6; i++){
-        let amt = parseFloat($(`#loan_amt0${i}`).val()).toFixed(2);
+        let amt = parseFloat($(`#loan_amt0${i}`).val()).toFixed(2); //fixed to two decimal points, like real money
         loans[i-1].loan_amount = amt;
       }
       let rate = parseFloat($("#loan_int01").val());
       for(i=0; i<5; i++){
         loans[i].loan_int_rate = rate;
       }
-
+      //then call the updateform function so we can actually see what we stored
       updateForm();
     }
   }
 
- let saveForm = () => {
-   localStorage.setItem(`as06`, JSON.stringify(loans));
+//function to save the form, has an appropriate button
+ function saveForm() {
+   localStorage.setItem('form', JSON.stringify(loans));
  }
 
- let loadForm = () => {
-  if(localStorage.getItem(`as06`) != null){
-     loans = JSON.parse(localStorage.getItem(`as06`));
+//fucntion to load the form, has an appropriate button
+ function loadForm() {
+   //if there's something stored
+  if(localStorage.getItem('form') != null){
+    //update loans array as the stored array
+     loans = JSON.parse(localStorage.getItem('form'));
+    //display the new form
      updateForm();
+    //otherwise display an appropriate alert 
   } else {
-     alert(`Error: no saved values`);
+     alert('Nothing to save');
   }
  }
 
+ 
+//Angular to populate payment table 
 var app = angular.module('myApp', []);
 app.controller('myCtrl', function($scope) {
-  $scope.payments =[];
+  //initiliaze scopes
+  $scope.payments =[]; //payment table 
+  $scope.lastPaymentRow = []; //the last row of the table 
+  //fucntion to populate table which can be called using ng 
   $scope.populate = function () {
-    updateForm();
-    let total = loanWithInterest;
-    let iRate = loans[0].loan_int_rate;
-    let r = iRate / 12;
-    let n = 11;
-    //loan payment formula
-    //https://www.thebalance.com/loan-payment-calculations-315564
-    let pay = 12 * (total / ((((1+r)**(n*12))-1)/(r *(1+r)**(n*12))));
+    let total = loanWithInterest; ///loan total retrieved from global variable 
+    let interest = loans[0].loan_int_rate; //interest rate
+    let r = interest / 12; //interets per month needed for the formula
+    let n = 11; //total years paying, n = n+1, needed for formula
+    //loan payment formula retrieved from https://www.calculatorsoup.com/calculators/financial/loan-calculator.php
+    let paymentFormula = 12 * (total / ((((1+r)**(n*12))-1)/(r *(1+r)**(n*12))));
+    //loop that populates the payments table, for 10 years, 0 to 10
     for (let i = 0; i < 10; i++) {
-      total -= pay //6500
-      let int = total * (iRate); 
+      total -= paymentFormula; //payment per year - total 
+      let int = total * (interest); //new interest because there is a new total
+      //store payment per year in the table
       $scope.payments[i]={
         "year":loans[4].loan_year + i + 1,
-        "payment": toMoney(pay), //toMoney(6500),
+        "payment": toMoney(paymentFormula), 
         "amt": toMoney(int),
         "ye": toMoney(total += int)
-      }
+      };
     }
-    $scope.payments[10] = {
-      "year":loans[4].loan_year + 11,
-      "payment": toMoney(total),
-      "amt": toMoney(0),
-      "ye":toMoney(0)
-    }
-  }
+    //last row of the table  aside because we have to said some values to 0 now because they are no longer applicable, this also makes setting the bgcolor easier because i have seperate scope now 
+    //and set payment to remaining total
+    $scope.lastPaymentRow[0] = {
+      "lyear":loans[4].loan_year + 11,
+      "lpayment": toMoney(total),
+      "lamt": toMoney(0),
+      "lye":toMoney(0),
+    };
+    //style to set the bgcolor to yellow when called, in this case the last row
+    $scope.lastPaymentColor = {
+    "background-color" : "yellow"
+    };
+  };
 });
